02598d224a6619d38439954272a5f205823971c9
Kento555 <shen_kento555@hotmail.com>
2025-04-05 12:43:14 +0800

    added lambda

---
+++ terraform/lambda.tf
@@ -0,0 +1,19 @@
+esource "aws_lambda_function" "this" {
+  function_name = var.name
+  handler       = var.handler
+  runtime       = var.runtime
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = var.source_path
+
+  source_code_hash = filebase64sha256(var.source_path)
+
+  environment {
+    variables = var.env_vars
+  }
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  timeout = var.timeout
+}
